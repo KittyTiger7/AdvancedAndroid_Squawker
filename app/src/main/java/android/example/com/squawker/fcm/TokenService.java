257f646b87bf070466fa673423a306aea1c82879
Avi Gagel <avigagel@Avis-MacBook-Pro.local>
2018-02-26 22:25:44 +0200

    Add a service to discover FCM Token

--- app/src/main/java/android/example/com/squawker/fcm/TokenService.java
+++ app/src/main/java/android/example/com/squawker/fcm/TokenService.java
@@ -1,8 +1,31 @@
 package android.example.com.squawker.fcm;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
 /**
  * Created by avigagel on 26/02/2018.
  */
 
-public class TokenService {
+public class TokenService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("onTokenRefresh", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+
+
 }
